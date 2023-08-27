2e9d46f403ead6070364c9594dd87c915e3885f0
emracatal <134175723+emracatal@users.noreply.github.com>
2023-08-27 21:44:22 +0300

    tamamlandı ancak tam anlamadım

--- frontend/App.js
+++ frontend/App.js
@@ -10,14 +10,14 @@ const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [geceModu, toggleGeceMode] = geceModuAc(false);
   const [login, setLogin, logOut] = localStorageKullan(false, "loggedInUser");
-  /*  useEffect(() => {
+  useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then((res) => setCoinData(res.data))
       .catch((err) => console.log(err));
-  }, []); */
+  }, []);
   return (
     <div className={geceModu ? "dark-mode App" : "App"}>
       <Navbar geceModu={geceModu} setGeceModu={toggleGeceMode} />
--- frontend/components/hooks/localStorageKullan.js
+++ frontend/components/hooks/localStorageKullan.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 
 function localStorageKullan(initialValue, key) {
   //const value = initialValue;
+
   const dataUpdateHandler = (newData) => {
     setValue(newData);
     localStorage.setItem(key, JSON.stringify(newData));
   };
 
-  const readFromLocalStorage = () => {
-    return JSON.parse(localStorage.getItem(key));
-  };
-
   const [value, setValue] = useState(() => {
-    return readFromLocalStorage() || initialValue;
+    const initialLocalStorageData = JSON.parse(localStorage.getItem(key));
+    console.log(initialLocalStorageData);
+
+    if (initialLocalStorageData === null) {
+      localStorage.setItem(key, JSON.stringify(initialValue));
+      return initialValue;
+    } else {
+      return initialLocalStorageData;
+    }
   });
 
   const clearLocalStorage = () => {
